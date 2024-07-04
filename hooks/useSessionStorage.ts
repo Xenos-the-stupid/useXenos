fe0b407e131c7f00299945eceefc1fb613a937da
xenos <acristyano22@gmail.com>
2024-07-04 15:52:55 +0100

    implement useSessionStorage

---
+++ hooks/useSessionStorage.ts
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const useSessionStorage = <T>(name: string, data?: T): [T | undefined, (newData: T) => void] => {
+  const [localStorageItem, setLocalStorageItem] = useState<T | undefined>(() => {
+    if (data) {
+      sessionStorage.setItem(name, JSON.stringify(data));
+      return data;
+    } else if (sessionStorage.getItem(name)) {
+      return JSON.parse(sessionStorage.getItem(name)!) as T;
+    }
+    return undefined;
+  });
+
+  const setItem = (newData: T) => {
+    sessionStorage.setItem(name, JSON.stringify(newData));
+    setLocalStorageItem(newData);
+  };
+
+  return [localStorageItem, setItem];
+};
+
+export default useSessionStorage 
