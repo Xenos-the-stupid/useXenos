dd70807ddb1f8886e5b6ee6d1714bc383b8f6f2f
xenos <acristyano22@gmail.com>
2024-07-06 11:54:29 +0100

    fix space

--- hooks/useWindowScroll/test-component.tsx
+++ hooks/useWindowScroll/test-component.tsx
@@ -3,7 +3,6 @@ import useWindowScroll from ".";
 
 const TestComponent = () => {
   const { setScrollX, setScrollY } = useWindowScroll({ behavior: "smooth" });
-
   return (
     <div style={{ width: "500vw", height: "400vh" }}>
       <button onClick={() => setScrollX(300)}>X</button>
