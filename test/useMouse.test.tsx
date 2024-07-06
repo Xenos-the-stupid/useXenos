ac554e523f9d606817f372e284505192f1632d88
xenos <acristyano22@gmail.com>
2024-07-06 12:14:39 +0100

    remove comments

--- test/useMouse.test.tsx
+++ test/useMouse.test.tsx
@@ -9,19 +9,4 @@ describe("useMouse", () => {
     expect(x).toBe(0);
     expect(y).toBe(0);
   });
-
-  // test("should update mouse position when mouse moves", () => {
-  //   global.document.dispatchEvent(
-  //     new MouseEvent("mousemove", {
-  //       clientX: 200,
-  //       clientY: 200,
-  //     })
-  //   );
-  //   const { result } = renderHook(() => useMouse());
-  //   const { x, y } = result.current;
-  //   expect(x).toBe(200);
-  //   expect(y).toBe(200);
-  // });
 });
-
-// same as useElementBounding.test
