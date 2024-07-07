9bcbf8d2ef61815d2b7022eed6812c4a5b7fa82d
xenos <acristyano22@gmail.com>
2024-07-07 11:50:56 +0100

    add error handling for `useMouseElement`

--- hooks/useMouseElement/index.ts
+++ hooks/useMouseElement/index.ts
@@ -4,6 +4,9 @@ const useMouseElement = (ref: RefObject<HTMLElement>) => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
+    if (!ref.current) {
+      throw new Error("Provide target element by providing ref for useMouseElement");
+    }
     const handleMouseMove = (event: MouseEvent) => {
       const { clientX, clientY } = event;
       setMousePosition({ x: clientX, y: clientY });
