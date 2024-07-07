f272e7a75e8188025ca589b02eeaee4730a9cc13
xenos <acristyano22@gmail.com>
2024-07-07 11:05:15 +0100

    update tests for `useElementBounding`

--- test/useElementBounding.test.tsx
+++ test/useElementBounding.test.tsx
@@ -1,5 +1,6 @@
 import { describe, expect, test } from "vitest";
 import useBoundingElement from "../hooks/useElementBounding";
+import { renderHook } from "@testing-library/react";
 
 describe("useBoundingElement", () => {
   test("useBoundingElement should be defined", () => {
@@ -8,5 +9,35 @@ describe("useBoundingElement", () => {
 
   test("useBoundingElement should be a function", () => {
     expect(typeof useBoundingElement).toBe("function");
+  });
+  test("", () => {
+    const ref = { current: document.createElement("div") };
+    ref.current.style.width = "100px";
+    ref.current.style.height = "100px";
+    ref.current.style.position = "absolute";
+    ref.current.style.left = "0px";
+    ref.current.style.top = "0px";
+    ref.current.style.right = "0px";
+    ref.current.style.bottom = "0px";
+    const { result, unmount } = renderHook(() => useBoundingElement(ref));
+    const { x, y, bottom, height, left, right, top, width } = result.current;
+    expect(x).toBe(0);
+    expect(y).toBe(0);
+    expect(bottom).toBe(0);
+    expect(top).toBe(0);
+    expect(height).toBe(0);
+    expect(width).toBe(0);
+    expect(left).toBe(0);
+    expect(right).toBe(0);
+
+    unmount();
+    expect(width).toBe(0);
+    expect(height).toBe(0);
+    expect(top).toBe(0);
+    expect(bottom).toBe(0);
+    expect(left).toBe(0);
+    expect(right).toBe(0);
+    expect(x).toBe(0);
+    expect(y).toBe(0);
   });
 });
