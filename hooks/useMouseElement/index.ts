f45be40c1dc4505ba6b16f37c782616d84a07486
xenos <acristyano22@gmail.com>
2024-07-07 18:20:15 +0100

    refactor `useMouseElement`

--- hooks/useMouseElement/index.ts
+++ hooks/useMouseElement/index.ts
@@ -1,21 +1,14 @@
 import { RefObject, useEffect, useState } from "react";
+import useEventListener from "../useEventListener";
 
 const useMouseElement = (ref: RefObject<HTMLElement>) => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    if (!ref.current) {
-      throw new Error("Provide target element by providing ref for useMouseElement");
-    }
-    const handleMouseMove = (event: MouseEvent) => {
-      const { clientX, clientY } = event;
-      setMousePosition({ x: clientX, y: clientY });
-    };
-    ref.current?.addEventListener("mousemove", handleMouseMove);
-    return () => {
-      ref.current?.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, [ref]);
+  // @ts-ignore
+  useEventListener(ref.current, "mousemove", (event: MouseEvent) => {
+    const { clientX, clientY } = event;
+    setMousePosition({ x: clientX, y: clientY });
+  });
 
   return { ...mousePosition };
 };
