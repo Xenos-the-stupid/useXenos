5f7dfa52f644b5cfdd9b3581fc416315d2b85189
xenos <acristyano22@gmail.com>
2024-07-07 11:32:20 +0100

    update tests for `useMouse`

--- test/useMouse.test.tsx
+++ test/useMouse.test.tsx
@@ -1,6 +1,7 @@
 import { renderHook } from "@testing-library/react-hooks";
 import useMouse from "../hooks/useMouse";
 import { describe, expect, test } from "vitest";
+import { fireEvent } from "@testing-library/dom";
 
 describe("useMouse", () => {
   test("should return initial mouse position", () => {
@@ -8,5 +9,16 @@ describe("useMouse", () => {
     const { x, y } = result.current;
     expect(x).toBe(0);
     expect(y).toBe(0);
+  });
+
+  test("update value when mouse moves", () => {
+    const { result, waitFor } = renderHook(() => useMouse());
+    fireEvent.mouseMove(window, { clientX: 200, clientY: 200 });
+
+    waitFor(() => {
+      const { x, y } = result.current;
+      expect(x).toBe(200);
+      expect(y).toBe(200);
+    });
   });
 });
