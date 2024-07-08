41bd748f4705923f4478fdd4913e041268f48c1d
xenos <acristyano22@gmail.com>
2024-07-08 13:31:17 +0100

    add new feature for useMouse `window hover state`

--- hooks/useMouse/index.ts
+++ hooks/useMouse/index.ts
@@ -3,12 +3,17 @@ import useEventListener from "../useEventListener";
 
 const useMouse = () => {
   const [coordinates, setCoordinates] = useState<Coordinates>({ x: 0, y: 0 });
+  const [isHovered, setIsHovered] = useState(false);
 
   // @ts-ignore
-  useEventListener(window, "pointermove", (event: MouseEvent) =>
-    setCoordinates({ x: event.clientX, y: event.clientY })
-  );
-  return coordinates;
+  useEventListener(window, "mousemove", (event: MouseEvent) => setCoordinates({ x: event.clientX, y: event.clientY }));
+  useEventListener(window, "mouseenter", () => {
+    setIsHovered(true);
+  });
+  useEventListener(window, "mouseleave", () => {
+    setIsHovered(false);
+  });
+  return { ...coordinates, isHovered };
 };
 
 type Coordinates = Readonly<{ x: number; y: number }>;
--- test/useMouse.test.ts
+++ test/useMouse.test.ts
@@ -1,8 +1,8 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import useMouse from "../hooks/useMouse";
 import { describe, expect, test } from "vitest";
 import { fireEvent } from "@testing-library/dom";
-import { act } from "react";
+import { waitFor } from "@testing-library/react";
 
 describe("useMouse", () => {
   test("should return initial mouse position", () => {
@@ -12,16 +12,43 @@ describe("useMouse", () => {
     expect(y).toBe(0);
   });
 
-  test("update value when mouse moves", () => {
-    const { result, waitFor } = renderHook(() => useMouse());
+  test("update value when mouse moves", async () => {
+    const { result } = renderHook(() => useMouse());
+
     act(() => {
       fireEvent.mouseMove(window, { clientX: 200, clientY: 200 });
     });
 
-    waitFor(() => {
+    await waitFor(() => {
       const { x, y } = result.current;
       expect(x).toBe(200);
       expect(y).toBe(200);
+    });
+  });
+
+  test("mouse should be in the window", async () => {
+    const { result } = renderHook(() => useMouse());
+
+    act(() => {
+      fireEvent.mouseEnter(window);
+    });
+
+    await waitFor(() => {
+      const { isHovered } = result.current;
+      expect(isHovered).toBe(true);
+    });
+  });
+
+  test("mouse should not be in the window", async () => {
+    const { result } = renderHook(() => useMouse());
+
+    act(() => {
+      fireEvent.mouseLeave(window);
+    });
+
+    await waitFor(() => {
+      const { isHovered } = result.current;
+      expect(isHovered).toBe(false);
     });
   });
 });
